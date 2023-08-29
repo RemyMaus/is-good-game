b053d604c749128975736556f835383ad1fa4260
RemyMaus <143537642+RemyMaus@users.noreply.github.com>
2023-08-29 20:58:47 +0200

    Added Console project

---
+++ GoodGame/GoodGame.Console/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GoodGame.Logic;
+
+Card card = new Card();
+FieldSurface fieldSurface = new FieldSurface(25);
+fieldSurface.SelectCardsRandomized(30);
+Console.WriteLine(fieldSurface.Cards.Count);
